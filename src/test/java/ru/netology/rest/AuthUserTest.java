7d3bfda78db33f3b09ac872b0de96d10b86c6944
Кухоцкая Екатерина <ekukhotskaya@mail.ru>
2022-08-18 08:30:16 +0300

    Reformat code

--- src/test/java/ru/netology/rest/AuthUserTest.java
+++ src/test/java/ru/netology/rest/AuthUserTest.java
@@ -46,10 +46,11 @@ public class AuthUserTest {
                 .shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Ошибка! Пользователь заблокирован"));
     }
+
     @Test
     public void shouldAlertWrongLoginIfActiveUser() {
         UserData user = generator.getNewUser("active", "en");
-        $("[data-test-id='login'] .input__control").setValue(user.getLogin()+1);
+        $("[data-test-id='login'] .input__control").setValue(user.getLogin() + 1);
         $("[data-test-id='password'] .input__control").setValue(user.getPassword());
         $("[data-test-id='action-login']").click();
         $("[data-test-id=error-notification]")
@@ -59,11 +60,12 @@ public class AuthUserTest {
                 .shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Неверно указан логин или пароль"));
     }
+
     @Test
     public void shouldAlertWrongPasswordIfActiveUser() {
         UserData user = generator.getNewUser("active", "en");
         $("[data-test-id='login'] .input__control").setValue(user.getLogin());
-        $("[data-test-id='password'] .input__control").setValue(user.getPassword()+2);
+        $("[data-test-id='password'] .input__control").setValue(user.getPassword() + 2);
         $("[data-test-id='action-login']").click();
         $("[data-test-id=error-notification]")
                 .shouldBe(Condition.visible)
@@ -72,10 +74,11 @@ public class AuthUserTest {
                 .shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Неверно указан логин или пароль"));
     }
+
     @Test
     public void shouldAlertWrongLoginIfBlockedUser() {
         UserData user = generator.getNewUser("blocked", "en");
-        $("[data-test-id='login'] .input__control").setValue(user.getLogin()+3);
+        $("[data-test-id='login'] .input__control").setValue(user.getLogin() + 3);
         $("[data-test-id='password'] .input__control").setValue(user.getPassword());
         $("[data-test-id='action-login']").click();
         $("[data-test-id=error-notification]")
@@ -85,11 +88,12 @@ public class AuthUserTest {
                 .shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Неверно указан логин или пароль"));
     }
+
     @Test
     public void shouldAlertWrongPasswordIfBlockedUser() {
-        UserData user = generator.getNewUser("active", "en");
+        UserData user = generator.getNewUser("blocked", "en");
         $("[data-test-id='login'] .input__control").setValue(user.getLogin());
-        $("[data-test-id='password'] .input__control").setValue(user.getPassword()+4);
+        $("[data-test-id='password'] .input__control").setValue(user.getPassword() + 4);
         $("[data-test-id='action-login']").click();
         $("[data-test-id=error-notification]")
                 .shouldBe(Condition.visible)
